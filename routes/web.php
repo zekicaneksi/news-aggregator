80cbe3c9093e1ab7d1c612545db920ee03a690c7
Zeki Can EKSI <zekicaneksi@gmail.com>
2023-12-10 12:59:25 +0300

    preparing guardian response

--- routes/web.php
+++ routes/web.php
@@ -47,69 +47,125 @@ Route::get('/fetch-news', function () {
 	    // Return the first 5 words
 	    return array_slice($words, 0, 5);
 	}
+	
+	function doesNewsExist($external_link){
+		$news = News::where('external_link', '=', substr($external_link,0,255))->first();
+		if ($news !== null) {
+		   	return $news;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	function prepareAndSaveNews($headline, $multimedia_url, $lead_paragraph, $category, $date, $source, $author, $external_link, $keywords){
 
-	function prepareNewsApi() {
-		$json_response = Storage::disk('local')->get('newsapi.json');
-		$json_response = json_decode($json_response, true);
-		
-		foreach ($json_response['articles'] as $key => $value) {
-		
-			//Skip if the news exists in the database
-			$doesNewsExist = News::where('external_link', '=', substr($value['url'],0,255))->first();
-			if ($doesNewsExist !== null) {
-			   continue;
-			}
-		
+			$category = Category::firstOrCreate(
+			    ['name' => $category]
+			);
+			
 			$source = Source::firstOrCreate(
-			    ['name' => $value['source']['name']]
+			    ['name' => $source]
 			);
 			
 			$author = Author::firstOrCreate(
-			    ['name' => $value['author']]
+			    ['name' => $author]
 			);
 			
-			$leadParagraph = $value['description'];
-			
-			$keywords = array();
-			$extracted_keywords = extractKeywords($leadParagraph);
+			$db_keywords = array();
 			
 			// Insert each keyword into database if not exists
-			foreach ($extracted_keywords as $extracted_keyword){
+			foreach ($keywords as $val_keyword){
 				$keyword = Keyword::firstOrCreate(
-				    ['name' => $extracted_keyword]
+				    ['name' => $val_keyword]
 				);
-				array_push($keywords, $keyword);
+				array_push($db_keywords, $keyword);
 			}
-			
-			$category = Category::firstOrCreate(
-			    ['name' => extractKeywords($value['title'])[0]]
-			);
-			
-			// Create and save the news
+
 			$news = new News;
-			$news->headline = substr($value['title'],0,255);
-			$news->multimedia_url = substr($value['urlToImage'],0,255);
-			$news->lead_paragraph = substr($leadParagraph,0,255);
+			$news->headline = substr($headline,0,255);
+			$news->multimedia_url = substr($multimedia_url,0,255);
+			$news->lead_paragraph = substr($lead_paragraph,0,255);
 			$news->category_id = $category->id;
-			$news->date = date('Y:m:d', strtotime($value['publishedAt']));
+			$news->date = date('Y:m:d', strtotime($date));
 			$news->source_id = $source->id;
 			$news->author_id = $author->id;
-			$news->external_link = substr($value['url'],0,255);
+			$news->external_link = substr($external_link,0,255);
 			
 			$news->save();
 			
 			// Insert news-keyword pairs
-			foreach ($keywords as $keyword) {
+			foreach ($db_keywords as $keyword) {
 				NewsKeywords::firstOrCreate([
 					'news_id' => $news->id,
 			    		'keyword_id' => $keyword->id
 		    		]);
 			}
+			
+			return $news;
+	}
+
+	function processNewsApi() {
+		$json_response = Storage::disk('local')->get('newsapi.json');
+		$json_response = json_decode($json_response, true);
+		
+		foreach ($json_response['articles'] as $key => $value) {
+		
+			$external_link = $value['url'];
+			
+			if (doesNewsExist($external_link) !== false) {
+				continue;
+			}
+			
+			$leadParagraph = $value['description'];
+
+			$extracted_keywords = extractKeywords($leadParagraph);
+			
+			$inserted_news = prepareAndSaveNews(
+				$value['title'],
+				$value['urlToImage'],
+				$value['description'],
+				extractKeywords($value['title'])[0],
+				$value['publishedAt'],
+				$value['source']['name'],
+				$value['author'],
+				$external_link,
+				$extracted_keywords
+			);
+		}
+	}
+	
+	function processGuardianApi(){
+		$json_response = Storage::disk('local')->get('guardian.json');
+		$json_response = json_decode($json_response, true);	
+		
+		foreach ($json_response['response']['results'] as $key => $value) {
+		
+			$external_link = $value['webUrl'];
+			
+			if (doesNewsExist($external_link) !== false) {
+				continue;
+			}
+			
+			$inserted_news = prepareAndSaveNews(
+				$value['webTitle'],
+				$value['fields']['thumbnail'],
+				$value['fields']['bodyText'],
+				$value['pillarName'],
+				$value['webPublicationDate'],
+				$value['fields']['publication'],
+				$value['fields']['byline'],
+				$external_link,
+				extractKeywords($value['fields']['headline'])
+			);
 		}
 	}
 	
 
-	prepareNewsApi();
+	//processNewsApi();
+	processGuardianApi();
+	
+	
     
     return 'OK';
 });

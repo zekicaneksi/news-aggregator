8919b8533e0a139598de440ec8e68253565c4cdc
Zeki Can EKSI <zekicaneksi@gmail.com>
2023-12-10 17:24:11 +0300

    moved from web to api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Classes\News;
+
+use Illuminate\Support\Facades\Http;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +20,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/fetch-news-local', function (Request $request) {
+
+    	function getJsonFromStorage($fileName){
+		$json_response = Storage::disk('local')->get('news_data/'.$fileName);
+		$json_response = json_decode($json_response, true);
+		
+		return $json_response;
+	}
+
+	News::processNewsApi(getJsonFromStorage('newsapi.json'));
+	News::processTimesApi(getJsonFromStorage('nytimes.json'));
+	
+	for ($i = 1; $i <= 6; $i++) {
+		News::processGuardianApi(getJsonFromStorage('guardian'.$i.'.json'));
+	} 
+	
+    
+	return 'OK';
+});
+
+Route::get('/fetch-news-remote', function (Request $request) {
+
+    	News::fetchDataFromRemote();
+    	
+	return 'OK';    	
 });
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Classes\News;
-
-use Illuminate\Support\Facades\Http;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,31 +15,5 @@ use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/fetch-news-local', function () {
-
-	function getJsonFromStorage($fileName){
-		$json_response = Storage::disk('local')->get('news_data/'.$fileName);
-		$json_response = json_decode($json_response, true);
-		
-		return $json_response;
-	}
-
-	News::processNewsApi(getJsonFromStorage('newsapi.json'));
-	News::processTimesApi(getJsonFromStorage('nytimes.json'));
-	
-	for ($i = 1; $i <= 6; $i++) {
-		News::processGuardianApi(getJsonFromStorage('guardian'.$i.'.json'));
-	} 
-	
-    
-	return 'OK';
-});
-
-Route::get('/fetch-news-remote', function () {
-    
-    	News::fetchDataFromRemote();
-	return 'OK';
 });
 
